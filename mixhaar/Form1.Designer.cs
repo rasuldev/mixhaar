20179c36df4e1071902c958af0132454770a5e27
Rasul <rasuldev@gmail.com>
2015-12-02 00:38:38 +0300

    graphbuilder initialized

--- mixhaar/Form1.Designer.cs
+++ mixhaar/Form1.Designer.cs
@@ -28,12 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.panelBottom.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.Controls.Add(this.numericUpDown1);
+            this.panelBottom.Location = new System.Drawing.Point(0, 451);
+            this.panelBottom.Size = new System.Drawing.Size(782, 100);
+            this.panelBottom.Controls.SetChildIndex(this.seriesListBox, 0);
+            this.panelBottom.Controls.SetChildIndex(this.numericUpDown1, 0);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(406, 6);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 551);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panelBottom.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
--- mixhaar/Form1.cs
+++ mixhaar/Form1.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DiscreteFunctionsPlots;
+using GraphBuilders;
 
 namespace mixhaar
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
+    public partial class Form1 : GraphBuilder2DForm
+    {        
+        Plot2D plotHaar = new Plot2D("Haar");
+        Plot2D plotMix = new Plot2D("Mixed from Haar");
+        
+        public Form1() : base(2)
         {
             InitializeComponent();
+            Height = Height + 1;
+            GetGraphBuilder(0).DrawPlot(plotHaar);
+            GetGraphBuilder(1).DrawPlot(plotMix);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //
         }
     }
 }
